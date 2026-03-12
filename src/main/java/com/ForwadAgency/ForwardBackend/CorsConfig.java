dd6a546276e4d25b91ef9e89d9c56b2c5d69ca59
praveen <praveendhanapal7@gmail.com>
2026-03-12 13:43:17 +0530

    Fix CORS

--- src/main/java/com/ForwadAgency/ForwardBackend/CorsConfig.java
+++ src/main/java/com/ForwadAgency/ForwardBackend/CorsConfig.java
@@ -1,8 +1,7 @@
-package com.ForwadAgency.ForwardBackend;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
@@ -13,9 +12,10 @@ public class CorsConfig {
 
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("*")
+                        .allowedOrigins("https://forwardagencies.netlify.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
         };
